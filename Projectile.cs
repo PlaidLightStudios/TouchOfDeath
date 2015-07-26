509493ca1accd0f9b31093dfad6aac04ba9ff8aa
PlaidLightStudios <Plaidlightstudios@gmail.com>
2015-07-25 23:03:14 -0400

    Projectile stuff

--- Projectile.cs
+++ Projectile.cs
@@ -9,14 +9,32 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Graphics;
 
 namespace TouchOfDeath
 {
     class Projectile
     {
-        public TextureView texture;
-        public float xDir;
-        public float ydir;
+        //texture
+        public Bitmap texture;
+        //direction of travel, Vector2
+        //origin point, Vector2
         bool isDead;
+
+        void projectile(string textName, bool isDead)
+        {
+            texture = BitmapFactory.DecodeFile(textName);
+        }
+
+        void projectile ()
+        {
+            texture = BitmapFactory.DecodeFile("projectile.png");
+            isDead = false;
+        }
+
+        void Draw()
+        {
+            Canvas canv = new Canvas(texture);
+        }
     }
 }
